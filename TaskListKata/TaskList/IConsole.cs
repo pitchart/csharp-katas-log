356082e2c1fc365f7281475c2ba333c123e6e86c
Julien VITTE <julien.vitte@cdbdx.biz>
2021-05-21 10:04:15 +0200

    feat(task-list): adds kata starter projects

---
+++ TaskListKata/TaskList/IConsole.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskListKata.TaskList
+{
+	public interface IConsole
+	{
+		string ReadLine();
+
+		void Write(string format, params object[] args);
+
+		void WriteLine(string format, params object[] args);
+
+		void WriteLine();
+	}
+}
