322157be59e8409dd4186ddd9dc576242a67f59f
Wael <wael.ajlani@ext.cdiscount.com>
2021-06-03 16:24:15 +0200

    Add theories

--- RomanNumeralKata/RomanNumerals.Test/ToRomanNumeralsConverterTest.cs
+++ RomanNumeralKata/RomanNumerals.Test/ToRomanNumeralsConverterTest.cs
@@ -17,7 +17,19 @@ namespace RomanNumerals.Test
         [Fact]
         public void Should_be_empty_for_zero()
         {
-            _converter.Convert(0).Should().Be("AA");
+            _converter.Convert(0).Should().Be("");
+        }
+        [Theory]
+        [InlineData(1,"I")]
+        [InlineData(5, "V")]
+        [InlineData(10, "X")]
+        [InlineData(50, "L")]
+        [InlineData(100, "C")]
+        [InlineData(500, "D")]
+        [InlineData(1000, "M")]
+        public void Should_convert_to_symbol(int number, string expected )
+        {
+            _converter.Convert(number).Should().Be(expected);
         }
     }
 }
