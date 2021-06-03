6d101aebd6605d1682ce886e01d32beeea77608f
Wael <wael.ajlani@ext.cdiscount.com>
2021-06-03 15:02:42 +0200

    Add symobls

--- RomanNumeralKata/RomanNumeral/ToRomanNumeralsConverter.cs
+++ RomanNumeralKata/RomanNumeral/ToRomanNumeralsConverter.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RomanNumeral
 {
     public class ToRomanNumeralsConverter
     {
+        readonly Dictionary<int, string> _romainNumbers = new Dictionary<int, string>
+        {
+            { 1,"I"},
+            { 5,"V"},
+            { 10,"X"},
+            { 50,"L"},
+            { 100,"C"},
+            { 500,"D"},
+            { 1000,"M"},
+        };
+        
+
         public string Convert(int arabic)
         {
-            throw new NotImplementedException();
+            return _romainNumbers.GetValueOrDefault(arabic, string.Empty);
         }
     }
 }
