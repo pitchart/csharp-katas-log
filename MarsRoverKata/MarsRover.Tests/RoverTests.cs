9754ab8ccab99aa15c72e0dd647b24558932c1ce
Alexandre Plaitant <alexandre.plaitant@cdbdx.biz>
2021-07-12 15:02:17 +0200

    Theory MOve Forward

--- MarsRoverKata/MarsRover.Tests/RoverTests.cs
+++ MarsRoverKata/MarsRover.Tests/RoverTests.cs
@@ -5,15 +5,19 @@ namespace MarsRover.Tests
 
     public class RoverTests
     {
-        [Fact]
-        public void ShouldMoveForward()
+        [Theory]
+        [InlineData("F", 'E', "3:2:E")]
+        [InlineData("FF", 'E', "4:2:E")]
+        [InlineData("FF", 'S', "2:0:S")]
+        [InlineData("FF", 'N', "2:4:N")]
+        [InlineData("FF", 'W', "0:2:W")]
+        public void ShouldMoveForward(string command, char direction, string expectedResult)
         {
-            var command = "F";
-            var rover = new Rover(2, 2, 'N');
+            var rover = new Rover(2, 2, direction);
 
             var result = rover.Move(command);
 
-            Assert.Equal("3:2:N", result);
+            Assert.Equal(expectedResult, result);
         }
     }
 
--- MarsRoverKata/MarsRover/Rover.cs
+++ MarsRoverKata/MarsRover/Rover.cs
@@ -1,22 +1,53 @@
-﻿namespace MarsRover
+﻿using System;
+
+namespace MarsRover
 {
 
     public class Rover
     {
         private int _x;
         private int _y;
-        private char _direction;
+        private readonly DirectionEnum _direction;
 
         public Rover(int x, int y, char direction)
         {
             this._x = x;
             this._y = y;
-            this._direction = direction;
+            Enum.TryParse(direction.ToString(), out _direction);
         }
 
         public object Move(string command)
         {
-            return "3:2:N";
+            var commands = command.ToCharArray();
+
+            foreach (var move in commands)
+            {
+                switch (_direction)
+                {
+                    case DirectionEnum.E:
+                        _x++;
+                        break;
+                    case DirectionEnum.W:
+                        _x--;
+                        break;
+                    case DirectionEnum.N:
+                        _y++;
+                        break;
+                    case DirectionEnum.S:
+                        _y--;
+                        break;
+                }
+            }
+
+            return $"{_x}:{_y}:{_direction}";
         }
+    }
+
+    public enum DirectionEnum
+    {
+        N,
+        E,
+        S,
+        W
     }
 }
