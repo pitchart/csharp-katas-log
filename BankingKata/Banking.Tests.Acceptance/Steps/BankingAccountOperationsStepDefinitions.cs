0baf773c1fc181420f50357eb7fd1b5e42ae7c87
Julien Lafargue <julien.lafargue@cdbdx.biz>
2021-07-19 11:31:44 +0200

    Test specflow

--- BankingKata/Banking.Tests.Acceptance/Steps/BankingAccountOperationsStepDefinitions.cs
+++ BankingKata/Banking.Tests.Acceptance/Steps/BankingAccountOperationsStepDefinitions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace Banking.Tests.Acceptance.Steps
 {
@@ -11,6 +12,10 @@ namespace Banking.Tests.Acceptance.Steps
 
         private readonly ScenarioContext _scenarioContext;
 
+        private Account _account = new Account();
+
+        private object _printStatementResult;
+
         public BankingAccountOperationsStepDefinitions(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
@@ -18,27 +23,41 @@ namespace Banking.Tests.Acceptance.Steps
 
         [Given(@"a client makes a deposit of (.*) on (.*)")]
         [Given(@"a deposit of (.*) on (.*)")]
-        public void GivenAClientMakesADepositOfOn(int p0, DateTime p1)
+        public void GivenAClientMakesADepositOfOn(int amount, string date)
         {
+            DateTime parseDate = ParseDate(date);
+
+            _account.Deposit(amount, parseDate);
+
             ScenarioContext.StepIsPending();
         }
 
         [When(@"she prints her bank statement")]
         public void WhenShePrintsHerBankStatement()
         {
+            _printStatementResult = _account.PrintStatement();
             ScenarioContext.StepIsPending();
         }
 
         [Then(@"she would see")]
         public void ThenSheWouldSee(string multilineText)
         {
+            Assert.Equal(multilineText, _printStatementResult);
             ScenarioContext.StepIsPending();
         }
 
         [Given(@"a withdrawal of (.*) on (.*)")]
-        public void GivenAWithdrawalOfOn(int p0, string p1)
+        public void GivenAWithdrawalOfOn(int amount, string date)
         {
+            DateTime parseDate = ParseDate(date);
+            _account.Withdraw(amount, parseDate);
             ScenarioContext.StepIsPending();
+        }
+
+        private static DateTime ParseDate(string date)
+        {
+            DateTime.TryParse(date, out DateTime parseDate);
+            return parseDate;
         }
     }
 }
--- BankingKata/Banking/Account.cs
+++ BankingKata/Banking/Account.cs
@@ -5,6 +5,20 @@ namespace Banking
 
     public class Account
     {
+        public void Deposit(int amount, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(int amount, DateTime parseDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object PrintStatement()
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
