e0d0126f2572683ec78a5ead6de80ecda8b493a4
Ilyass Aazmi <ilyass.aazmi@cdbdx.biz>
2021-07-26 14:52:42 +0200

    Create RoverBuilder

--- MarsRoverKata/MarsRover.Tests/RoverTests.cs
+++ MarsRoverKata/MarsRover.Tests/RoverTests.cs
@@ -74,4 +74,34 @@ namespace MarsRover.Tests
 
     }
 
+    public class RoverBuilder
+    {
+        private int x = 2;
+        private int y = 2;
+        private char direction = 'N';
+
+        public Rover Build()
+        {
+            return new Rover(x, y, direction);
+        }
+
+        public RoverBuilder LandingAt(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            return this;
+        }
+
+        public RoverBuilder Facing(char direction)
+        {
+            this.direction = direction;
+            return this;
+        }
+
+        public static RoverBuilder Create()
+        {
+            return new RoverBuilder();
+        }
+    }
+
 }
