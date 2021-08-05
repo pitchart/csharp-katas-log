da5ff1b54bcfd78dd4c3a2310290dbafa908c4b4
Julien VITTE <julien.vitte@cdbdx.biz>
2021-08-05 11:15:48 +0200

    feat(tennis): adds tennis refactoring kata
    (cherry picked from commit 6e93c56b5b191df895f1c7741d02f54e1bdf1edd)

---
+++ Tennis/Tests/Tennis2Tests.cs
@@ -0,0 +1,10 @@
+namespace Tennis.Tests
+{
+
+    public class Tennis2Tests : TennisTestCases
+    {
+        public Tennis2Tests() : base(new TennisGame2("player1", "player2"))
+        {}
+    }
+
+}
