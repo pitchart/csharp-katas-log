7a15dddc4fb123cbc1ffb92ca1ebcc592d48b3d6
Alexandre Plaitant <alexandre.plaitant@cdiscount.com>
2021-11-08 14:36:02 +0100

    Equality

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Tennis.ScoreTennis3;
 
 namespace Tennis
@@ -9,8 +10,6 @@ namespace Tennis
         private readonly string _player1Name;
         private readonly string _player2Name;
 
-        private readonly string[] _tennisScore = { "Love", "Fifteen", "Thirty", "Forty" };
-
         public TennisGame3(string player1Name, string player2Name)
         {
             this._player1Name = player1Name;
@@ -19,25 +18,17 @@ namespace Tennis
 
         public string GetScore()
         {
-            if (_player1Point < 4 && _player2Point < 4 && _player1Point + _player2Point < 6)
+            if (_player1Point < 4 && _player2Point < 4 &&  _player1Point != _player2Point)
             {
-                var player1Score = _tennisScore[_player1Point];
-                if (_player1Point == _player2Point)
-                    return player1Score + "-All";
-                
-                return player1Score + "-" + _tennisScore[_player2Point];
+                return TennisScore.Get(_player1Point) + "-" + TennisScore.Get(_player2Point);
             }
 
-            if (_player1Point == _player2Point)
-                return "Deuce";
+            return new Equality(new Advantage(new Win())).GetScore(_player1Point, _player2Point, GetLeadingPlayer());
+        }
 
-            string advantagePlayerName;
-            if (_player1Point > _player2Point)
-                advantagePlayerName = _player1Name;
-            else
-                advantagePlayerName = _player2Name;
-
-            return new Advantage(new Win()).GetScore(_player1Point, _player2Point, advantagePlayerName);
+        private string GetLeadingPlayer()
+        {
+            return _player1Point > _player2Point ? _player1Name : _player2Name;
         }
 
         public void WonPoint(string playerName)
@@ -48,6 +39,43 @@ namespace Tennis
                 this._player2Point += 1;
         }
 
+    }
+
+    public class Equality : IScore
+    {
+        private readonly IScore _next;
+        
+        public Equality(IScore next)
+        {
+            _next = next;
+        }
+
+        public string GetScore(int playerOnePoint, int playerTwoPoint, string playerName)
+        {
+            if (playerOnePoint == playerTwoPoint && playerOnePoint + playerTwoPoint < 6) 
+                return TennisScore.Get(playerOnePoint) + "-All";
+
+            if (playerOnePoint == playerTwoPoint)
+                return "Deuce";
+
+            return _next.GetScore(playerOnePoint, playerTwoPoint, playerName);
+        }
+    }
+
+    public static class TennisScore
+    {
+        private static readonly Dictionary<int, string> Scores = new()
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"},
+        };
+        
+        public static string Get(int playerPoint)
+        {
+            return Scores[playerPoint];
+        }
     }
 
     public interface IScore
