f6837c4cf451156530612c2d65b10579253e041c
yann.courtel <yann.courtel@ext.cdiscount.com>
2021-11-12 14:51:15 +0100

    feat(bowling): add basic score with no special throw (7 points)

--- BowlingKata/Bowling.Test/BowlingTest.cs
+++ BowlingKata/Bowling.Test/BowlingTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Bowling.Test
@@ -6,17 +5,33 @@ namespace Bowling.Test
 
     public class BowlingTest
     {
+        private readonly Bowling game;
+
+        public BowlingTest()
+        {
+            game = new Bowling();
+        }
+
         [Fact]
         public void Should_score_zero_when_all_rolls_go_to_the_gutter()
         {
-            Bowling game = new Bowling();
-
             for (int i = 0; i < 20; i++)
                 game.Roll(0);
 
             var score = game.Score();
 
             Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void Should_score_40_when_all_rolls_knocks_two_pins()
+        {
+            for (int i = 0; i < 20; i++)
+                game.Roll(2);
+
+            var score = game.Score();
+
+            Assert.Equal(40, score);
         }
     }
 
--- BowlingKata/Bowling/Bowling.cs
+++ BowlingKata/Bowling/Bowling.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Bowling
 {
 
     public class Bowling
     {
+        private readonly List<int> scores = new List<int>(); // 1
+
         public void Roll(int pins)
         {
-
+            scores.Add(pins); // 3
         }
 
         public int Score()
         {
-            return 0; // 1
+            return scores.Sum(); // 3
         }
     }
 
