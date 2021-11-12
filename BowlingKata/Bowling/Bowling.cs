455ab25890d604fcbf7e12c74ce37b14c6887cb7
Julien VITTE <julien.vitte@cdbdx.biz>
2021-11-12 15:05:09 +0100

    feat(bowling): green -> add spare and 5 pins throw (34 points)

--- BowlingKata/Bowling/Bowling.cs
+++ BowlingKata/Bowling/Bowling.cs
@@ -15,7 +15,17 @@ namespace Bowling
 
         public int Score()
         {
-            return scores.Sum(); // 3
+            int score = 0; // 1
+            for (int i = 0; i < 20; i = i + 2)// 6
+            {
+                int frameScore = scores[i] + scores[i + 1]; // 1 + 3
+                score += frameScore; // 7
+                if (frameScore == 10) // 4
+                {
+                    score += scores[i + 2]; // 7
+                }
+            }
+            return score; // 1
         }
     }
 
