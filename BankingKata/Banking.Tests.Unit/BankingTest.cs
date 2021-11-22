5e688a1b5810427e7a13bdcd7a09c95f54b758fd
Marie-Eugenie Mechain <MarieEugenie.mechain@cdbdx.biz>
2021-11-22 14:20:54 +0100

    WIP : print deposit

--- BankingKata/Banking.Tests.Unit/BankingTest.cs
+++ BankingKata/Banking.Tests.Unit/BankingTest.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace Banking.Tests.Unit
@@ -19,6 +21,22 @@ namespace Banking.Tests.Unit
             // Assert
             var ExpectedOutput = "date       ||   credit ||    debit ||  balance";
             output.Should().Be(ExpectedOutput);
+        }
+
+        [Fact]
+        public void Should_print_deposits()
+        {
+            //Arrange
+            var account = new Account();
+            account.Deposit(1, DateTime.Parse("2021-11-22"));
+            var printer = new Printer();
+
+            //Act
+            string output = printer.Print(account.GetStatement());
+
+            //Assert
+            var ExpectedOutput = "22-11-2021 ||     1.00 ||          ||     1.00";
+            output.Split(Environment.NewLine).Last().Should().Be(ExpectedOutput);
         }
     }
 }
