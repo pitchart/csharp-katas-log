3b0032d89071a851fdcf394baee0ef117926015e
Laurent <laurent.martins@cdiscount.com>
2021-11-22 14:45:05 +0100

    olé

--- BankingKata/Banking.Tests.Unit/BankingTest.cs
+++ BankingKata/Banking.Tests.Unit/BankingTest.cs
@@ -23,19 +23,22 @@ namespace Banking.Tests.Unit
             output.Should().Be(ExpectedOutput);
         }
 
-        [Fact]
-        public void Should_print_deposits()
+
+        [Theory]
+        [InlineData(1, "2021-11-22", "22-11-2021 ||     1.00 ||          ||     1.00")]
+        [InlineData(2, "2021-11-22", "22-11-2021 ||     2.00 ||          ||     2.00")]
+        public void Should_print_deposits(int amount, string date, string transactionLine)
         {
             //Arrange
             var account = new Account();
-            account.Deposit(1, DateTime.Parse("2021-11-22"));
+            account.Deposit(amount, DateTime.Parse(date));
             var printer = new Printer();
 
             //Act
             string output = printer.Print(account.GetStatement());
 
             //Assert
-            var ExpectedOutput = "22-11-2021 ||     1.00 ||          ||     1.00";
+            var ExpectedOutput = transactionLine;
             output.Split(Environment.NewLine).Last().Should().Be(ExpectedOutput);
         }
     }
