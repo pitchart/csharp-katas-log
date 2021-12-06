da6bc8ac2eaa1ba89404b45617828cc01d02838d
alexandre.casteill <alexandre.casteill@ext.cdiscount.com>
2021-12-06 11:48:09 +0100

    first tests

--- DiamondKata/Diamond.Tests/DiamondTest.cs
+++ DiamondKata/Diamond.Tests/DiamondTest.cs
@@ -6,9 +6,32 @@ namespace Diamond.Tests
 
     public class DiamondTest
     {
+        private Diamond diamond = new Diamond(); 
+        
         [Fact]
-        public void Test1()
+        public void Write_A_Diamond()
         {
+            //Arrange
+            char letter = 'A';
+            
+            //Act
+            string result = diamond.Print(letter);
+            
+            //Assert
+            Assert.Equal("A",result);
+        }
+
+        [Fact]
+        public void Write_B_Diamond()
+        {
+            //Arrange
+            char letter = 'B';
+            
+            //Act
+            string result = diamond.Print(letter);
+            
+            //Assert
+            Assert.StartsWith("AB",result);
         }
     }
 
--- DiamondKata/Diamond/Diamond.cs
+++ DiamondKata/Diamond/Diamond.cs
@@ -5,6 +5,20 @@ namespace Diamond
 
     public class Diamond
     {
+        private  char startChar = 'A';
+        
+        public string Print(char c)
+        {
+            string result = "";
+            char letter = startChar;
+            while (letter.CompareTo(c) <= 0)
+            {
+                result += letter;
+                letter ++;
+            }
+
+            return result;
+        }
     }
 
 }
