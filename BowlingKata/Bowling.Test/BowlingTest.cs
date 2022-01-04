36cb173fa821fc9230cf7458861b59829e7bc2da
Abdelouahab Assklou <abdelouahab.assklou@cdbdx.biz>
2022-01-04 15:32:03 +0100

    start bowling test spare + strike case

--- BowlingKata/Bowling.Test/BowlingTest.cs
+++ BowlingKata/Bowling.Test/BowlingTest.cs
@@ -3,13 +3,89 @@ using Xunit;
 
 namespace Bowling.Test
 {
-
     public class BowlingTest
     {
         [Fact]
-        public void Test1()
+        public void Score_should_be_zero_when_all_missing_tries()
         {
+            Bowling bowling = new Bowling();
+
+            for (int i = 0; i < 20; i++)
+            {
+                bowling.Roll(0);
+            }
+
+            var score = bowling.Score();
+
+            Assert.Equal(0, score);
+        }
+
+        [Fact]
+        public void Score_should_be_tweenty_pins_all_rolls_hits_one_pin()
+        {
+            Bowling bowling = new Bowling();
+
+            for (int i = 0; i < 20; i++)
+            {
+                bowling.Roll(1);
+            }
+
+            var score = bowling.Score();
+
+            Assert.Equal(20, score);
+        }
+
+        [Fact]
+        public void Score_should_have_bonus_for_spare()
+        {
+            Bowling bowling = new Bowling();
+
+            bowling.Roll(3);
+            bowling.Roll(7);
+
+            for (int i = 0; i < 18; i++)
+            {
+                bowling.Roll(1);
+            }
+
+            var score = bowling.Score();
+
+            Assert.Equal(29, score);
+        }
+
+        [Fact]
+        public void Score_should_have_bonus_for_strike()
+        {
+            Bowling bowling = new Bowling();
+
+            bowling.Roll(10);
+
+            for (int i = 0; i < 18; i++)
+            {
+                bowling.Roll(1);
+            }
+
+            var score = bowling.Score();
+
+            Assert.Equal(30, score);
+        }
+
+        [Fact]
+        public void Score_should_have_bonus_for_successive_strikes()
+        {
+            Bowling bowling = new Bowling();
+
+            bowling.Roll(10);
+            bowling.Roll(10);
+
+            for (int i = 0; i < 16; i++)
+            {
+                bowling.Roll(1);
+            }
+
+            var score = bowling.Score();
+
+            Assert.Equal(49, score);
         }
     }
-
 }
--- BowlingKata/Bowling/Bowling.cs
+++ BowlingKata/Bowling/Bowling.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bowling
 {
-
     public class Bowling
     {
+        private List<int> _scores = new List<int>();
+
+        public void Roll(int pins)
+        {
+            _scores.Add(pins);
+
+            if (pins == 10)
+            {
+                _scores.Add(0);
+            }
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            for (int i = 0; i < 20; i += 2)
+            {
+                score += _scores[i] + _scores[i + 1];
+
+                if (IsStrike(i))
+                {
+                    score += _scores[i + 2] + _scores[i + 3];
+                }
+                else if (IsSpare(i))
+                {
+                    score += _scores[i + 2];
+                }
+            }
+            return score;
+        }
+
+        private bool IsStrike(int i)
+        {
+            return _scores[i] == 10;
+        }
+
+        private bool IsSpare(int turn)
+        {
+            return (_scores[turn] + _scores[turn + 1]) == 10;
+        }
     }
 
 }
