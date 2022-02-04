2192d9a565d77f90f3d399a63c3015515161b8e9
Meidi Kadri <meidi.kadri@cdbdx.biz>
2022-02-04 14:35:59 +0100

    start

--- BowlingKata/Bowling.Test/BowlingTest.cs
+++ BowlingKata/Bowling.Test/BowlingTest.cs
@@ -3,13 +3,24 @@ using Xunit;
 
 namespace Bowling.Test
 {
-
     public class BowlingTest
     {
         [Fact]
-        public void Test1()
+        public void WhenNoPinShot_ShouldReturnZero()
         {
+            // Arrange
+            Bowling bowling = new Bowling();
+
+            // Act
+            for (int i = 0; i < 20; i++)
+            {
+                bowling.Roll(0);
+            }
+
+            int total = bowling.GetScore();
+
+            // Assert
+            Assert.Equal(total, 0);
         }
     }
-
 }
