4b4d22f5c42da998c8102d4fda5879cb8746ade1
Julien VITTE <julien.vitte@cdbdx.biz>
2022-02-11 15:16:04 +0100

    feat(bowling): inits score calculator

--- BowlingKata/Bowling.Test/BowlingTest.cs
+++ BowlingKata/Bowling.Test/BowlingTest.cs
@@ -6,9 +6,71 @@ namespace Bowling.Test
 
     public class BowlingTest
     {
+        private readonly Bowling _bowling = new Bowling();
+
         [Fact]
-        public void Test1()
+        public void ScoreShouldBeZeroWhenAllRollsGoesToGutter()
         {
+
+
+            // ACT
+            for (int i = 1; i <= 20; i++)
+            {
+                _bowling.Roll(0);
+            }
+
+            // ASSERT
+            Assert.Equal(0, _bowling.Score());
+        }
+
+        [Fact]
+        public void ScoreShouldBeTheSumOfAllPinsWhenNoBonus()
+        {
+
+            // ACT
+            for (int i = 1; i <= 20; i++)
+            {
+                _bowling.Roll(2);
+            }
+
+            // ASSERT
+            Assert.Equal(40, _bowling.Score());
+        }
+
+        [Fact]
+        public void ScoreShouldBeTheSumOfAllPinsAndBonusWhenOneSpareHasBeenHit()
+        {
+
+            // ACT
+
+            _bowling.Roll(2);
+            _bowling.Roll(8);
+
+            for (int i = 3; i <= 20; i++)
+            {
+                _bowling.Roll(2);
+            }
+
+            // ASSERT
+            Assert.Equal(48, _bowling.Score());
+        }
+
+
+        [Fact]
+        public void ScoreShouldBeTheSumOfAllPinsAndBonusWhenOneStrikeHasBeenHit()
+        {
+
+            // ACT
+
+            _bowling.Roll(10);
+
+            for (int i = 3; i <= 20; i++)
+            {
+                _bowling.Roll(2);
+            }
+
+            // ASSERT
+            Assert.Equal(50, _bowling.Score());
         }
     }
 
--- BowlingKata/Bowling/Bowling.cs
+++ BowlingKata/Bowling/Bowling.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bowling
 {
 
     public class Bowling
     {
+        private List<int> _rolls = new List<int>();
+
+        public void Roll(int pins)
+        {
+            _rolls.Add(pins);
+
+            if (pins == 10)
+            {
+                _rolls.Add(0);
+            }
+        }
+
+        public int Score()
+        {
+            var total = 0;
+
+            for (int i = 0; i < _rolls.Count; i += 2)
+            {
+                var turnScore = _rolls[i] + _rolls[i + 1];
+
+                total += turnScore;
+
+                if (_rolls[i] == 10)
+                {
+                    total += _rolls[i + 2] + _rolls[i + 3];
+                }
+                else if (TurnIsSpare(turnScore))
+                {
+                    total += _rolls[i + 2];
+                }
+            }
+
+            return total;
+        }
+
+        private bool TurnIsSpare(int turnScore)
+        {
+            return turnScore == 10;
+        }
     }
 
 }
