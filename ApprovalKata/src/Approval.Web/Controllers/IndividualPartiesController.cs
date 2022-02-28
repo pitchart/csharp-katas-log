0b55830f50b1fdb749bead77a4d6e6cdb771e2d7
Mohammed Sajid <mohammed.sajid@cdbdx.biz>
2022-02-28 12:03:35 +0100

    #Integration Tests with Verify

---
+++ ApprovalKata/src/Approval.Web/Controllers/IndividualPartiesController.cs
@@ -0,0 +1,15 @@
+﻿using Approval.Shared.ReadModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Approval.Web.Controllers
+{
+    [Route("individualParties")]
+    public class IndividualPartiesController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IndividualParty[]> GetIndividualParties()
+        {
+            return Array.Empty<IndividualParty>();
+        }
+    }
+}
---
+++ ApprovalKata/tests/Approval.Tests/Integration/IndividualPartiesControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using Approval.Shared.ReadModels;
+using Approval.Shared.SalesForce;
+using Approval.Web;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Approval.Tests.Integration
+{
+    public class IndividualPartiesControllerTests : AppFactory
+    {
+        private HttpClient _httpClient;
+        public IndividualPartiesControllerTests()
+        {
+            _httpClient = CreateClient();
+        }
+
+        [Fact]
+        public async Task Should_Map_PersonAccounts_To_IndividualParties()
+        {
+            //Arrange
+            var alCapOne = DataBuilder.AlCapone();
+            var mesrine = DataBuilder.Mesrine();
+
+            //Act
+            var result = await _httpClient.GetAsync("/individualParties");
+
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var individualParties = Assert.IsAssignableFrom<IndividualParty[]>(await result.Content.ReadAsStringAsync());
+
+            AssertAlCapOne(individualParties[0], alCapOne);
+            AssertMesrine(individualParties[1], mesrine);
+        }
+
+        private static void AssertAlCapOne(IndividualParty individualParty, PersonAccount personAccount)
+        {
+            individualParty.BirthCity.Should().Be(personAccount.CityOfBirth__pc);
+            individualParty.BirthDate.Should().Be(25.January(1899));
+            individualParty.FirstName.Should().Be(personAccount.FirstName);
+            individualParty.LastName.Should().Be(personAccount.LastName);
+            individualParty.MiddleName.Should().Be(personAccount.MiddleName);
+            individualParty.PepMep.Should().Be(false);
+            individualParty.Title.Should().Be(personAccount.Salutation);
+            individualParty.Documents.First().DocumentType.Should().Be(personAccount.LegalDocumentName1__c);
+            individualParty.Documents.First().ExpirationDate.Should().Be(5.January(2000));
+            individualParty.Documents.First().Number.Should().Be(personAccount.LegalDocumentNumber1__c);
+            individualParty.Gender.Should().Be(Gender.Male);
+        }
+
+        private static void AssertMesrine(IndividualParty individualParty, PersonAccount personAccount)
+        {
+            individualParty.BirthCity.Should().Be(personAccount.CityOfBirth__pc);
+            individualParty.BirthDate.Should().Be(28.December(1936));
+            individualParty.FirstName.Should().Be(personAccount.FirstName);
+            individualParty.LastName.Should().Be(personAccount.LastName);
+            individualParty.MiddleName.Should().Be(personAccount.MiddleName);
+            individualParty.PepMep.Should().Be(true);
+            individualParty.Title.Should().Be(personAccount.Salutation);
+            individualParty.Documents.First().DocumentType.Should().Be(personAccount.LegalDocumentName1__c);
+            individualParty.Documents.First().ExpirationDate.Should().Be(30.September(2020));
+            individualParty.Documents.First().Number.Should().Be(personAccount.LegalDocumentNumber1__c);
+            individualParty.Documents.Last().DocumentType.Should().Be(personAccount.LegalDocumentName2__c);
+            individualParty.Documents.Last().ExpirationDate.Should().Be(23.December(1990));
+            individualParty.Documents.Last().Number.Should().Be(personAccount.LegalDocumentNumber2__c);
+            individualParty.Gender.Should().Be(Gender.Male);
+        }
+    }
+}
