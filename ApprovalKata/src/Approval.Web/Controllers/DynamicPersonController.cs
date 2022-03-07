c5f6f83fb47e22b29e716240af4626fc09afcd31
alexandre.casteill <alexandre.casteill@ext.cdiscount.com>
2022-03-07 11:58:04 +0100

    add dynamic person test and controller

---
+++ ApprovalKata/src/Approval.Web/Controllers/DynamicPersonController.cs
@@ -0,0 +1,15 @@
+﻿using Approval.Shared.ReadModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Approval.Web.Controllers
+{
+    [Route("dynamicPerson")]
+    public class DynamicPersonController : Controller
+    {
+        [HttpGet]
+        public ActionResult<DynamicPerson> Index()
+        {
+            return DataBuilder.Montana();
+        }
+    }
+}
---
+++ ApprovalKata/tests/Approval.Tests/Integration/DynamicPersonControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Approval.Shared.ReadModels;
+using FluentAssertions;
+using Newtonsoft.Json;
+using VerifyXunit;
+using Xunit;
+using static VerifyXunit.Verifier;
+
+namespace Approval.Tests.Integration
+{
+    [UsesVerify]
+    public class DynamicPersonControllerTests : AppFactory
+    {
+        private HttpClient _httpClient;
+        public DynamicPersonControllerTests()
+        {
+            _httpClient = CreateClient();
+        }
+        
+
+        [Fact]
+        public Task Should_return_dynamic_person()
+        {
+            var result = _httpClient.GetAsync("/dynamicPerson").Result;
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var montana =
+                JsonConvert.DeserializeObject<DynamicPerson>(result.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+            return Verify(montana);
+
+        }
+    }
+}
