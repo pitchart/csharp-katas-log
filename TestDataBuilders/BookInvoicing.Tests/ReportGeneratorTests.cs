6f35cff7b5901328fc945977bb644e9c20e4af1c
Julien VITTE <julien.vitte@cdbdx.biz>
2022-05-09 12:20:57 +0200

    feat(tests-data-builders): adds starting tests without builders
    (cherry picked from commit feabd83f829cb9b26587d61cdc6348d5d06a1fe3)

--- TestDataBuilders/BookInvoicing.Tests/ReportGeneratorTests.cs
+++ TestDataBuilders/BookInvoicing.Tests/ReportGeneratorTests.cs
@@ -13,13 +13,78 @@ namespace BookInvoicing.Tests
         [Fact]
         public void ShouldComputeTotalAmount_WithoutDiscount_WithoutTaxExchange()
         {
-            // Todo : add tests for invoice with no discount and no tax exchange
+            // Arrange
+            var inMemoryRepository = OverrideRepositoryForTests();
+            ReportGenerator generator = new ReportGenerator();
+
+            var book = new EducationalBook(
+                "Clean Code", 25, new Author(
+                    "Uncle Bob", new Country(
+                        "USA", Currency.UsDollar, Language.English
+                        )
+                    ),
+                Language.English, Category.Computer
+            );
+
+            var purchasedBook = new PurchasedBook(book, 2);
+
+            Invoice invoice = new Invoice("John Doe", new Country("USA", Currency.UsDollar, Language.English));
+            invoice.AddPurchasedBooks(new List<PurchasedBook> { purchasedBook });
+
+            // Act
+            inMemoryRepository.AddInvoice(invoice);
+
+            // Assert
+            Assert.Equal(50, generator.GetTotalAmount());
+            Assert.Equal(1, generator.GetNumberOfIssuedInvoices());
+            Assert.Equal(2, generator.GetTotalSoldBooks());
+
+            ResetTestsRepository();
         }
 
         [Fact]
         public void ShouldComputeTotalAmount_WithDiscount_WithTaxExchanges()
         {
-            // Todo : add tests for invoice with discount and tax exchanges
+            // Arrange
+            var inMemoryRepository = OverrideRepositoryForTests();
+            ReportGenerator generator = new ReportGenerator();
+
+            var book = new Novel("A mysterious adventure fiction", 35.5, new Author(
+                    "Some Guy", new Country(
+                        "France", Currency.Euro, Language.French
+                        )
+                    ),
+                Language.English, new List<Genre> {Genre.Mystery, Genre.AdventureFiction }
+            );
+
+            var purchasedBook = new PurchasedBook(book, 3);
+
+            var invoice = new Invoice("John Doe", new Country(
+                "Germany", Currency.Euro, Language.German
+            ));
+            invoice.AddPurchasedBooks(new List<PurchasedBook> { purchasedBook });
+
+            // Act
+            inMemoryRepository.AddInvoice(invoice);
+
+            // Assert
+            Assert.Equal(106.5, generator.GetTotalAmount());
+            Assert.Equal(1, generator.GetNumberOfIssuedInvoices());
+            Assert.Equal(3, generator.GetTotalSoldBooks());
+
+            ResetTestsRepository();
+        }
+
+        private InMemoryRepository OverrideRepositoryForTests()
+        {
+            InMemoryRepository inMemoryRepository = new InMemoryRepository();
+            MainRepository.Override(inMemoryRepository);
+            return inMemoryRepository;
+        }
+
+        private void ResetTestsRepository()
+        {
+            MainRepository.Reset();
         }
     }
 }
