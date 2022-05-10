103a6aa911b1cc07e1396a94575907bd183269c7
Abdelouahab Assklou <abdelouahab.assklou@cdbdx.biz>
2022-05-10 15:35:01 +0200

    add test for null and blank result case

--- ElectionsKata/Elections.Tests/ElectionsTests.cs
+++ ElectionsKata/Elections.Tests/ElectionsTests.cs
@@ -40,6 +40,114 @@ public class ElectionsTests
     }
 
     [Fact]
+    public Task Should_run_without_districts_when_blank_result()
+    {
+        var list = new Dictionary<string, List<string>>
+        {
+            ["District 1"] = new List<string> { "Bob", "Anna", "Jess", "July" },
+            ["District 2"] = new List<string> { "Jerry", "Simon" },
+            ["District 3"] = new List<string> { "Johnny", "Matt", "Carole" }
+        };
+
+        var elections = new Elections(list, false);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
+
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+        elections.VoteFor("Matt", "---", "District 3");
+        elections.VoteFor("Jess", "xxx", "District 1");
+
+        var results = elections.Results();
+
+        // Add approval tests here
+        return Verify(results);
+    }
+
+    [Fact]
+    public Task Should_run_without_districts_when_not_have_null_and_blank_result()
+    {
+        var list = new Dictionary<string, List<string>>
+        {
+            ["District 1"] = new List<string> { "Bob", "Anna", "Jess", "July" },
+            ["District 2"] = new List<string> { "Jerry", "Simon" },
+            ["District 3"] = new List<string> { "Johnny", "Matt", "Carole" }
+        };
+
+        var elections = new Elections(list, false);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
+
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+
+        var results = elections.Results();
+
+        // Add approval tests here
+        return Verify(results);
+    }
+
+    [Fact]
+    public Task Should_run_without_districts_when_null_result()
+    {
+        var list = new Dictionary<string, List<string>>
+        {
+            ["District 1"] = new List<string> { "Bob", "Anna", "Jess", "July" },
+            ["District 2"] = new List<string> { "Jerry", "Simon" },
+            ["District 3"] = new List<string> { "Johnny", "Matt", "Carole" }
+        };
+
+        var elections = new Elections(list, false);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
+
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+        elections.VoteFor("Matt", "", "District 3");
+        elections.VoteFor("Jess", "", "District 1");
+
+        var results = elections.Results();
+
+        // Add approval tests here
+        return Verify(results);
+    }
+
+    [Fact]
+    public Task Should_run_with_districts_when_not_have_null_and_blank_result()
+    {
+        var list = new Dictionary<string, List<string>>
+        {
+            ["District 1"] = new List<string> { "Bob", "Anna", "Jess", "July" },
+            ["District 2"] = new List<string> { "Jerry", "Simon" },
+            ["District 3"] = new List<string> { "Johnny", "Matt", "Carole" }
+        };
+        var elections = new ElectionsWithDistrict(list);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
+
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+        elections.VoteFor("July", "Jerry", "District 1");
+
+        var results = elections.Results();
+
+        // Add approval tests here
+        return Verify(results);
+    }
+
+    [Fact]
     public Task Should_run_with_districts()
     {
         var list = new Dictionary<string, List<string>>
