d09b5c5a87631d18c07bec25a4556f23385747c5
Julien Lafargue <julien.lafargue@cdbdx.biz>
2022-05-16 11:31:59 +0200

    Real life refacto with try

--- LanguageExtKata/language-ext-kata/Account/AccountService.cs
+++ LanguageExtKata/language-ext-kata/Account/AccountService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
 
 namespace language_ext.kata.Account;
 
@@ -20,34 +23,37 @@ public class AccountService
 
     public string Register(Guid id)
     {
-        try
-        {
-            var user = _userService.FindById(id);
+        Try<string> result = Try(() => TweetForNewUser(id));
 
-            if (user == null) return null;
-
-            var accountId = _twitterService.Register(user.Email, user.Name);
-
-            if (accountId == null) return null;
-
-            var twitterToken = _twitterService.Authenticate(user.Email, user.Password);
-
-            if (twitterToken == null) return null;
-
-            var tweetUrl = _twitterService.Tweet(twitterToken, "Hello I am " + user.Name);
-
-            if (tweetUrl == null) return null;
-
-            _userService.UpdateTwitterAccountId(id, accountId);
-            _businessLogger.LogSuccessRegister(id);
-
-            return tweetUrl;
-        }
-        catch (Exception ex)
+        return result.IfFail(ex =>
         {
             _businessLogger.LogFailureRegister(id, ex);
 
             return null;
-        }
+        });
+    }
+
+    private string TweetForNewUser(Guid id)
+    {
+        var user = _userService.FindById(id);
+
+        if (user == null) return null;
+
+        var accountId = _twitterService.Register(user.Email, user.Name);
+
+        if (accountId == null) return null;
+
+        var twitterToken = _twitterService.Authenticate(user.Email, user.Password);
+
+        if (twitterToken == null) return null;
+
+        var tweetUrl = _twitterService.Tweet(twitterToken, "Hello I am " + user.Name);
+
+        if (tweetUrl == null) return null;
+
+        _userService.UpdateTwitterAccountId(id, accountId);
+        _businessLogger.LogSuccessRegister(id);
+
+        return tweetUrl;
     }
 }
