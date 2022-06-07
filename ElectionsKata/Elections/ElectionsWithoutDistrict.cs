b4d952ac7a2aaecb82f83da9cab3a36ebde107ad
Abdelouahab Assklou <abdelouahab.assklou@cdbdx.biz>
2022-06-07 15:17:27 +0200

    refacto + clean code

--- ElectionsKata/Elections/ElectionsWithoutDistrict.cs
+++ ElectionsKata/Elections/ElectionsWithoutDistrict.cs
@@ -1,38 +1,28 @@
 ﻿using Elections.Interfaces;
-using System.Collections.Generic;
 using System.Globalization;
 
 namespace Elections
 {
     public class ElectionsWithoutDistrict : IElections
     {
-        private Dictionary<string, List<string>> _list;
-        private readonly List<string> _officialCandidates = new List<string>();
+        private Dictionary<string, List<string>> _electorsByDistrict;
+        private readonly List<string> _officialCandidates = new();
 
-        private readonly Dictionary<string, int> voteByCandidate = new Dictionary<string, int>();
-        private readonly List<string> _urne = new List<string>();
+        private readonly List<string> _urne = new();
 
-
-        public ElectionsWithoutDistrict(Dictionary<string, List<string>> list)
+        public ElectionsWithoutDistrict(Dictionary<string, List<string>> electorsByDistrict)
         {
-            _list = list;
+            _electorsByDistrict = electorsByDistrict;
         }
 
         public void AddCandidate(string candidate)
         {
             _officialCandidates.Add(candidate);
-
-            voteByCandidate.Add(candidate, 0);
         }
 
         public void VoteFor(string elector, string candidate, string electorDistrict)
         {
             _urne.Add(candidate);
-        }
-
-        private void VoteForExistingCandidate(string candidate)
-        {
-            voteByCandidate[candidate] += 1;
         }
 
         public Dictionary<string, string> Results()
@@ -52,7 +42,7 @@ namespace Elections
             var nullResult = (float)nullVotes * 100 / nbVotes;
             results["Null"] = string.Format(cultureInfo, "{0:0.00}%", nullResult);
 
-            var nbElectors = _list.Sum(kv => kv.Value.Count);
+            var nbElectors = _electorsByDistrict.Sum(kv => kv.Value.Count);
             var abstentionResult = 100 - (float)nbVotes * 100 / nbElectors;
             results["Abstention"] = string.Format(cultureInfo, "{0:0.00}%", abstentionResult);
 
@@ -61,36 +51,17 @@ namespace Elections
 
         private (Dictionary<string, string> results, int nbVotes, int nullVotes, int blankVotes, int nbValidVotes) ResultWithoutDistrict(CultureInfo cultureInfo)
         {
-            var results = new Dictionary<string, string>();
             var nbVotes = _urne.Count;
             var nullVotes = _urne.Count(vote => !vote.Equals(string.Empty) && !_officialCandidates.Contains(vote));
             var blankVotes = _urne.Count(vote => vote.Equals(string.Empty));
             var nbValidVotes = _urne.Count(vote => _officialCandidates.Contains(vote));
 
-            /*
-             * on prend tout les bulletins valide
-             * on groupe par candidat
-             * on les comptes
-             */
-            foreach (var vote in _urne)
+            var results = _officialCandidates.ToDictionary(candidat => candidat, candidat =>
             {
-                if (voteByCandidate.ContainsKey(vote))
-                {
-                    VoteForExistingCandidate(vote);
-                }
-            }
-
-            /*
-             * on calcule le pourcentage de vote
-             * on formate le pourcentage
-             */
-            foreach (var candidateVotes in voteByCandidate)
-            {
-                int nbVote = candidateVotes.Value;
-                var precent = nbVote * 100 / nbValidVotes;
-                string candidateName = candidateVotes.Key;
-                results[candidateName] = string.Format(cultureInfo, "{0:0.00}%", precent);
-            }
+                var nbVote = _urne.Count(vote => vote.Equals(candidat));
+                var percent = nbVote * 100 / nbValidVotes;
+                return string.Format(cultureInfo, "{0:0.00}%", percent);
+            });
 
             return (results, nbVotes, nullVotes, blankVotes, nbValidVotes);
         }
