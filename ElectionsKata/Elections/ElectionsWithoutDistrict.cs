4750509db84dd7a65e688c15cb8df06e2dc60112
Julien Lafargue <julien.lafargue@cdbdx.biz>
2022-06-14 14:59:51 +0200

    Refacto Results

--- ElectionsKata/Elections/ElectionsWithoutDistrict.cs
+++ ElectionsKata/Elections/ElectionsWithoutDistrict.cs
@@ -27,20 +27,23 @@ namespace Elections
 
         public Dictionary<string, string> Results()
         {
-            var results = new Dictionary<string, string>();
-            var nbVotes = 0;
-            var nullVotes = 0;
-            var blankVotes = 0;
-            var nbValidVotes = 0;
             var cultureInfo = new CultureInfo("fr-fr");
 
-            (results, nbVotes, nullVotes, blankVotes, nbValidVotes) = ResultWithoutDistrict(cultureInfo);
+            var nbVotes = _urne.Count;
+            var nbValidVotes = _urne.Count(IsValid);
 
-            var blankResult = (float)blankVotes * 100 / nbVotes;
-            results["Blank"] = string.Format(cultureInfo, "{0:0.00}%", blankResult);
+            var results = _officialCandidates.ToDictionary(candidat => candidat, candidat =>
+            {
+                var nbVote = _urne.Count(vote => vote.Equals(candidat));
+                var percent = nbVote * 100 / nbValidVotes;
+                return FormatResult(cultureInfo, percent);
+            });
 
-            var nullResult = (float)nullVotes * 100 / nbVotes;
-            results["Null"] = string.Format(cultureInfo, "{0:0.00}%", nullResult);
+            var blankResult = ComputePercentage(nbVotes, IsBlank);
+            results["Blank"] = FormatResult(cultureInfo, blankResult);
+
+            var nullResult = ComputePercentage(nbVotes, IsNull);
+            results["Null"] = FormatResult(cultureInfo, nullResult);
 
             var nbElectors = _electorsByDistrict.Sum(kv => kv.Value.Count);
             var abstentionResult = 100 - (float)nbVotes * 100 / nbElectors;
@@ -49,21 +52,24 @@ namespace Elections
             return results;
         }
 
-        private (Dictionary<string, string> results, int nbVotes, int nullVotes, int blankVotes, int nbValidVotes) ResultWithoutDistrict(CultureInfo cultureInfo)
+        private float ComputePercentage(int nbVotes, Func<string, bool> filter)
         {
-            var nbVotes = _urne.Count;
-            var nullVotes = _urne.Count(IsNull);
-            var blankVotes = _urne.Count(vote => vote.Equals(string.Empty));
-            var nbValidVotes = _urne.Count(vote => _officialCandidates.Contains(vote));
+            return (float)_urne.Count(filter) * 100 / nbVotes;
+        }
 
-            var results = _officialCandidates.ToDictionary(candidat => candidat, candidat =>
-            {
-                var nbVote = _urne.Count(vote => vote.Equals(candidat));
-                var percent = nbVote * 100 / nbValidVotes;
-                return string.Format(cultureInfo, "{0:0.00}%", percent);
-            });
+        private static string FormatResult(CultureInfo cultureInfo, float result)
+        {
+            return string.Format(cultureInfo, "{0:0.00}%", result);
+        }
 
-            return (results, nbVotes, nullVotes, blankVotes, nbValidVotes);
+        private bool IsValid(string vote)
+        {
+            return _officialCandidates.Contains(vote);
+        }
+
+        private static bool IsBlank(string vote)
+        {
+            return vote.Equals(string.Empty);
         }
 
         private bool IsNull(string vote)
