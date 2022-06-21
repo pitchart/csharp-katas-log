76bca169b7bc9c67bc161226b37613676c75a59a
Julien Lafargue <julien.lafargue@cdbdx.biz>
2022-06-21 15:32:58 +0200

    Add test for ElectionWithDistrict

--- ElectionsKata/Elections.Tests/ElectionsTests.cs
+++ ElectionsKata/Elections.Tests/ElectionsTests.cs
@@ -177,7 +177,37 @@ public class ElectionsTests
         return Verify(results);
     }
 
-    [Fact(Skip ="Feature doesn't exist anymore")]
+    [Fact]
+    public Task Should_run_with_districts_with_different_nomber_of_null_and_blank_vote()
+    {
+        var list = new Dictionary<string, List<string>>
+        {
+            ["District 1"] = new List<string> { "Bob", "Anna", "Jess", "July" },
+            ["District 2"] = new List<string> { "Jerry", "Simon" },
+            ["District 3"] = new List<string> { "Johnny", "Matt", "Carole" }
+        };
+        var elections = new ElectionsWithDistrict(list);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
+
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+        elections.VoteFor("Matt", "Donald", "District 3");
+        elections.VoteFor("Jess", "Joe", "District 1");
+        elections.VoteFor("July", "Jerry", "District 1");
+        elections.VoteFor("Simon", "nullVote", "District 2");
+        elections.VoteFor("Carole", "", "District 3");
+
+        var results = elections.Results();
+
+        // Add approval tests here
+        return Verify(results);
+    }
+
+    [Fact(Skip = "Feature doesn't exist anymore")]
     public void Should_run_with_districts_and_ignore_elector_vote_in_another_district()
     {
         var list = new Dictionary<string, List<string>>
