24fd48507109f55a0133d7c1468211a41369a182
Marie-Eugenie Mechain <MarieEugenie.mechain@cdbdx.biz>
2022-07-12 12:01:56 +0200

    WIP : spaces in diamond

--- DiamondKata/Diamond.Tests/DiamondTest.cs
+++ DiamondKata/Diamond.Tests/DiamondTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using FluentAssertions;
 using FsCheck;
@@ -26,7 +27,7 @@ namespace Diamond.Tests
             var result = Diamond.Display(letter);
 
             //Assert
-            var lines = result.Split('\n');
+            var lines = result.Split(Environment.NewLine);
             var middleLine = lines[lines.Length / 2];
             middleLine.Should().Contain(letter.ToString());
         }
@@ -38,7 +39,7 @@ namespace Diamond.Tests
             var result = Diamond.Display(letter);
 
             //Assert
-            var lines = result.Split('\n');
+            var lines = result.Split(Environment.NewLine);
             var firstLine = lines[0];
             firstLine.Should().Contain("A");
         }
@@ -46,18 +47,33 @@ namespace Diamond.Tests
         [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
         public void Should_have_consecutive_letters(char letter)
         {
-            //Arrange
-
             //Act
             var result = Diamond.Display(letter);
 
             //Assert
             var lines = result
-                .Split('\n')
+                .Split(Environment.NewLine)
                 .Select(s => s.Trim(' '))
                 .Select(s => s[0]);
 
-            var expected = new string[] {};
+            var expected = Enumerable.Range((int)'A', (int)letter - 'A' + 1).Select(i => (char)i);
+            expected = expected.Concat(expected.Reverse().Skip(1));
+            lines.Should().BeEquivalentTo(expected);
+        }
+
+        [Property(Arbitrary = new[] { typeof(LetterGenerator) })]
+        public void Should_have_a_decreasing_number_of_outer_spaces(char letter)
+        {
+            //Act
+            var result = Diamond.Display(letter);
+
+            //Assert
+            var lines = result
+                .Split(Environment.NewLine)
+                .TakeWhile(line => !line.Contains(letter))
+                .Select(x => x.ToCharArray().TakeWhile(c => c == ' ').Count());
+
+            var expected = Enumerable.Range(1, (int)letter - 'A').Reverse();
             lines.Should().BeEquivalentTo(expected);
         }
     }
--- DiamondKata/Diamond/Diamond.cs
+++ DiamondKata/Diamond/Diamond.cs
@@ -1,13 +1,21 @@
-﻿namespace Diamond
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Diamond
+{
     public class Diamond
     {
         public static string Display(char letter)
         {
-            if (letter == 'A') return "A";
-            return $"A\n{letter}\nA";
+            IEnumerable<char> diamond = Pyramid(letter);
+            diamond = diamond.Concat(diamond.Reverse().Skip(1));
+            return String.Join(Environment.NewLine, diamond);
+        }
+
+        private static IEnumerable<char> Pyramid(char letter)
+        {
+            return Enumerable.Range((int)'A', (int)letter - (int)'A' + 1).Select(i => (char)i);
         }
     }
-
 }
