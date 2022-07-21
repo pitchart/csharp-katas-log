b3cec973500e2b9272d9a0308a249d72a4d41118
khaoula.sabri@cdbdx.biz <khaoula.sabri@ext.cdiscount.com>
2022-07-21 15:03:36 +0200

    refacto calcul du resultat de vote des candidat officiels

--- ElectionsKata/Elections/ElectionsWithoutDistrict.cs
+++ ElectionsKata/Elections/ElectionsWithoutDistrict.cs
@@ -4,30 +4,15 @@ namespace Elections
 {
     public class ElectionsWithoutDistrict : Elections
     {
-        protected readonly List<int> _votesWithoutDistricts = new List<int>();
         protected readonly List<string> _urne = new List<string>();
 
         public ElectionsWithoutDistrict(Dictionary<string, List<string>> list) : base(list)
         {
         }
-        public override void AddCandidate(string candidate)
-        {
-            base.AddCandidate(candidate);
-            _votesWithoutDistricts.Add(0);
-        }
+
         public override void VoteFor(string elector, string candidate, string electorDistrict)
         {
             _urne.Add(candidate);
-            if (_candidates.Contains(candidate))
-            {
-                var index = _candidates.IndexOf(candidate);
-                _votesWithoutDistricts[index] = _votesWithoutDistricts[index] + 1;
-            }
-            else
-            {
-                _candidates.Add(candidate);
-                _votesWithoutDistricts.Add(1);
-            }
         }
 
         public override Dictionary<string, string> Results()
@@ -36,23 +21,14 @@ namespace Elections
             var nbVotes = _urne.Count();
             var nullVotes = _urne.Count(vote => IsVoteNull(vote));
             var blankVotes = _urne.Count(vote => vote.Equals(string.Empty));
-            var nbValidVotes = 0;
+            var nbValidVotes = _urne.Count(vote => _officialCandidates.Contains(vote));
 
-            for (var i = 0; i < _officialCandidates.Count; i++)
+            
+            foreach (var candidate in _officialCandidates)
             {
-                var index = _candidates.IndexOf(_officialCandidates[i]);
-                nbValidVotes += _votesWithoutDistricts[index];
-            }
-
-            for (var i = 0; i < _votesWithoutDistricts.Count; i++)
-            {
-                float candidateResult = GetPercent(_votesWithoutDistricts[i], nbValidVotes);
-                var candidate = _candidates[i];
-
-                if (_officialCandidates.Contains(candidate))
-                {
-                    results[candidate] = FormatResult(candidateResult);
-                }
+                var nombreVote = _urne.Count(vote => vote.Equals(candidate));
+                float candidateResult = GetPercent(nombreVote, nbValidVotes);
+                results[candidate] = FormatResult(candidateResult);
             }
 
             var blankResult = GetPercent(blankVotes, nbVotes);
