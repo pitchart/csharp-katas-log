31e4f2c6ede032ac9908f9c82148c5d248b4df25
khaoula.sabri@cdbdx.biz <khaoula.sabri@ext.cdiscount.com>
2022-08-11 15:02:13 +0200

    Add Urnetests

---
+++ ElectionsKata/Elections.Tests/UrneTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elections.Tests
+{
+    public class UrneTests
+    {
+        [Fact]
+        public void Should_return_number_of_votes()
+        {
+            var urne = new Urne();
+            urne.Vote("1");
+            urne.Vote("2");
+            urne.Vote("3");
+
+            Assert.Equal(3, urne.GetTotalVote());
+        }
+
+        [Fact]
+        public void Should_return_number_of_blank_votes()
+        {
+            var urne = new Urne();
+            urne.Vote("NotBlank");
+            urne.Vote("");
+            urne.Vote("NotBlank");
+
+            Assert.Equal(1, urne.GetNumberBlankVotes());
+        }
+    }
+}
--- ElectionsKata/Elections/Urne.cs
+++ ElectionsKata/Elections/Urne.cs
@@ -4,17 +4,17 @@
     {
         private List<string> ListVotes = new List<string>();
 
-        internal void Vote(string candidate) => ListVotes.Add(candidate);
+        public void Vote(string candidate) => ListVotes.Add(candidate);
 
-        internal int GetTotalVote() => ListVotes.Count();
+        public int GetTotalVote() => ListVotes.Count();
 
-        internal int GetNumberBlankVotes() => ListVotes.Count(vote => vote.Equals(string.Empty));
+        public int GetNumberBlankVotes() => ListVotes.Count(vote => vote.Equals(string.Empty));
 
-        internal int GetNumberNullVotes(List<string> officialCandidates)
+        public int GetNumberNullVotes(List<string> officialCandidates)
         {
             return ListVotes.Count(vote => !(vote.Equals(string.Empty) || officialCandidates.Contains(vote)));
         }
-        internal int GetNumberValidVotes(List<string> officialCandidates) => ListVotes.Count(vote => officialCandidates.Contains(vote));
-        internal int GetNumberVotesFor(string candidate) => ListVotes.Count(vote => vote.Equals(candidate));
+        public int GetNumberValidVotes(List<string> officialCandidates) => ListVotes.Count(vote => officialCandidates.Contains(vote));
+        public int GetNumberVotesFor(string candidate) => ListVotes.Count(vote => vote.Equals(candidate));
     }
 }
