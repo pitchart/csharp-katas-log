04ed862263edba6fb6a62b9a5c4afc7e5209605a
Ilyass Aazmi <ilyass.aazmi@cdbdx.biz>
2022-10-04 14:55:24 +0200

    Overload addition operator for Amount

--- OrderShippingKata/Order_Shipping_Test/PriceTest.cs
+++ OrderShippingKata/Order_Shipping_Test/PriceTest.cs
@@ -12,33 +12,52 @@ namespace OrderShippingTest
         [Fact]
         public void Should_round_the_amount_with_two_digits()
         {
-            Price price = new(150.163m);
+            Amount amount = new(150.163m);
 
-            Assert.Equal(150.17m, price.RoundedValue);
+            Assert.Equal(150.17m, amount.RoundedValue);
         }
 
         [Fact]
         public void Should_be_divisible_by_decimal_value()
         {
-            Price price = new(150.163m);
+            Amount amount = new(150.163m);
 
-            var result = price / 10m;
+            var result = amount / 10m;
             Assert.Equal(15.02m, result.RoundedValue);
+        }
+
+        [Fact]
+        public void Should_not_be_divisible_by_zero()
+        {
+            Assert.Throws<DivideByZeroException>(() => new Amount(150.163m)/0);
+        }
+
+        [Fact]
+        public void Should_be_additionable()
+        {
+            Amount amount_30_25 = new(30.25m);
+            Amount amount_25 = new(25m);
+
+
+            var result = amount_30_25 + amount_25;
+            Assert.Equal(55.25m, result.RoundedValue);
         }
     }
 
-    public class Price
+    public class Amount
     {
         private readonly decimal _amount;
 
-        public Price(decimal amount)
+        public Amount(decimal amount)
         {
             _amount = amount;
         }
 
         public decimal RoundedValue => decimal.Round(_amount, 2, MidpointRounding.ToPositiveInfinity);
 
-        public static Price operator /(Price price, decimal value) => new(price._amount / value);
+        public static Amount operator /(Amount amount, decimal value) => new(amount._amount / value);
+
+        public static Amount operator +(Amount amount, Amount other) => new(amount._amount + other._amount);
     }
 
 }
