29219c99ebf304656b14003d4b0132a6f04f5822
Ahmed Amine Mahamani <ahmedamine.mahamani@cdbdx.biz>
2022-11-15 12:09:10 +0100

    add more units

--- NombresEnFrancais/NumberInFrench.cs
+++ NombresEnFrancais/NumberInFrench.cs
@@ -4,7 +4,28 @@
     {
         public static string GetNumberInFrench(int number)
         {
-            return "un";
+            switch (number)
+            {
+                case 1:
+                    return "un";
+                case 2:
+                    return "deux";
+                case 3:
+                    return "trois";
+                case 4:
+                    return "quatre";
+                case 5:
+                    return "cinq";
+                case 6:
+                    return "six";
+                case 7:
+                    return "sept";
+                case 8:
+                    return "huit";
+                case 9:
+                    return "neuf";
+            }
+            return "zero";
         }
     }
 }
--- NombresEnFrancaisTests/GetNumberInFrenchTests.cs
+++ NombresEnFrancaisTests/GetNumberInFrenchTests.cs
@@ -6,6 +6,16 @@ namespace NombresEnFrancaisTests
     public class GetNumberInFrenchTests
     {
         [Fact]
+        public void GetNumberInFrench_When0_ReturnsZero()
+        {
+            int zero = 0;
+
+            string result = NumberInFrench.GetNumberInFrench(zero);
+
+            Assert.Equal("zero", result);
+        }
+
+        [Fact]
         public void GetNumberInFrench_When1_ReturnsUn()
         {
             int un = 1;
@@ -13,6 +23,22 @@ namespace NombresEnFrancaisTests
             string result = NumberInFrench.GetNumberInFrench(un);
 
             Assert.Equal("un", result);
+        }
+
+        [Theory]
+        [InlineData(2, "deux")]
+        [InlineData(3, "trois")]
+        [InlineData(4, "quatre")]
+        [InlineData(5, "cinq")]
+        [InlineData(6, "six")]
+        [InlineData(7, "sept")]
+        [InlineData(8, "huit")]
+        [InlineData(9, "neuf")]
+        public void GetNumberInFrench_WhenUnit_ReturnsUnitAsString(int number, string expected)
+        {
+            string result = NumberInFrench.GetNumberInFrench(number);
+
+            Assert.Equal(expected, result);
         }
     }
 }
