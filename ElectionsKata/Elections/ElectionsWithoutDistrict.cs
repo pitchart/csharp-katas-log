13cfbd3e94182745b92642f5a9c3f693358feb51
Laurent Martins <laurent.martins@cdbdx.biz>
2024-03-04 14:49:33 +0100

    clean ElectionsWithoutDistrict

--- ElectionsKata/Elections/ElectionsWithoutDistrict.cs
+++ ElectionsKata/Elections/ElectionsWithoutDistrict.cs
@@ -7,8 +7,7 @@ namespace Elections
         private readonly List<string> _officialCandidates = new List<string>();
         private readonly Dictionary<string, List<string>> _list;
         private readonly ResultFormater _formater;
-        private readonly Dictionary<string, int> _urn = new();
-        private readonly Urn _newUrn = new();
+        private readonly Urn _urn = new();
 
         public ElectionsWithoutDistrict(Dictionary<string, List<string>> list)
         {
@@ -19,29 +18,16 @@ namespace Elections
         internal void AddCandidate(string candidate)
         {
             _officialCandidates.Add(candidate);
-            _urn.Add(candidate, 0);
         }
 
         internal void VoteFor(string elector, string candidate, string electorDistrict)
         {
-            if (_urn.ContainsKey(candidate))
-            {
-                _urn[candidate] += 1;
-            }
-            else
-            {
-                _urn.Add(candidate, 1);
-            }
-
-            _newUrn.VoteFor(candidate);
+            _urn.VoteFor(candidate);
         }
-
-        //TODO => guard constructeur VoteCounting
-
 
         internal Dictionary<string, string> ComputeResults()
         {
-            var voteCounting = _newUrn.CountVotes(_officialCandidates);
+            var voteCounting = _urn.CountVotes(_officialCandidates);
             var nbElectors = _list.Sum(kv => kv.Value.Count);
             var percentResult = voteCounting.ToPercentResult(nbElectors);
             return _formater.FormatResults(percentResult);
